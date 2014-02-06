3a484245ce78562e072367b8f58d56d3c665b7ef
drfinlayscott <drfinlayscott@gmail.com>
2014-02-06 17:48:44 +0100

    Fixed the other cross machine problem with remainder check

--- R/project.r
+++ R/project.r
@@ -142,7 +142,8 @@ setMethod('project', signature(object='MizerParams', effort='array'),
 
         # Make the MizerSim object with the right size
         # We only save every t_save steps
-        if (!all((t_save %% dt)  == 0))
+        #if (!all((t_save %% dt)  == 0))
+        if(!all.equal((t_max - floor(t_max / dt) * dt),0))
             stop("t_save must be divisible by dt with no remainder")
         t_dimnames_index <- as.integer(seq(from = 1+ ((t_save-1) / dt), to = length(time_effort_dt), by = t_save/dt))
         t_dimnames_index <- t_dimnames_index[t_dimnames_index>0]
