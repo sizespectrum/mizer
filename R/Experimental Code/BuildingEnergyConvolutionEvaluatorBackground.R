8c3228e7518e78d5cab09d84cecdad548af58721
Richard Southwell <Richard Southwell>
2017-02-06 11:32:31 +0000

    Working version after some git confusion

--- R/Experimental Code/BuildingEnergyConvolutionEvaluatorBackground.R
+++ R/Experimental Code/BuildingEnergyConvolutionEvaluatorBackground.R
@@ -12,67 +12,35 @@ library(methods)
 library(plyr)
 library(reshape2)
 
-params <- set_community_model(z0 = 0.1, f0 = 0.7, alpha = 0.2, recruitment = 4e7)
+params <- set_community_model(z0 = 0.1, f0 = 0.7, alpha = 0.2, recruitment = 4e7,
+                              sigma = 2, no_w=100)
 object <- params 
 sim <- project(params, t_max=1)
 
 # extract n_pp and n from sim object 
 n_pp <- sim@n_pp[1, ]
 n <- sim@n[1, , ]
-
 # sim@n[time,species, weight ]
 # we need to get species index back even though there is only one species
 dim(n) <- c(1, length(n))
 
-params@interaction
-
-summary(params)
-
-params@pred_kernel
+mizerResult <- rowSums(sweep(object@pred_kernel,3,object@dw_full*object@w_full*n_pp,"*", check.margin=FALSE),dims=2)
 
 w0 <- object@w[1]
 
 Beta <- log(object@species_params$beta)
 sigma <- object@species_params$sigma
-wFull <- params@w_full 
-xFull <- log(wFull/w0)
- s <- exp(-(xFull - Beta)^2/(2*sigma^2))/(sqrt(2*pi)*sigma)
-#s <- exp(-(xFull +Beta)^2/(2*sigma^2))
+wFull <- params@w_full
+xFull <- log(wFull)
 dx <- xFull[2]-xFull[1]
-f <- n_pp*wFull*wFull
+s <- exp(-(xFull - xFull[1] - Beta)^2/(2*sigma^2))
+
+#f <- object@dw_full*object@w_full*n_pp
+f <- wFull*wFull*n_pp
 
 energy <- dx*Re(fft(fft(s)*fft(f), inverse=TRUE)/length(s))
-
-plot(xFull,energy)
-
-#now result is about 5 times too big
-
-########### compare #########
-
-mizerResult <- rowSums(sweep(object@pred_kernel,3,object@dw_full*object@w_full*n_pp,"*", check.margin=FALSE),dims=2)
-dim(mizerResult)
-
-length(object@w_full)
-
-plot(log(object@w/w0), mizerResult)
-
-dim(object@pred_kernel)
-
-###################
-
-#plot(xFull[82:130],energy[82:130])
-
-plot((1:length(mizerResult)),mizerResult)
-lines(energy[60:130])
-
-lines(mizerResult)
+idx_sp <- (length(object@w_full) - length(object@w) + 1):length(object@w_full)
+plot(log(object@w),energy[idx_sp])
+lines(log(object@w), mizerResult)
 
 
-mizerResult[1]
-
-which(energy>31824)
-
-plot(xFull,s)
-Beta
-
-# does this match pred_kernel ?
