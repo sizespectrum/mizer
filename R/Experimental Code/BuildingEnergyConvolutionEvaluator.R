9465bbebcbc232733e5b9a5aedf4ede4c477fade
Gustav Delius <gustav.delius@gmail.com>
2017-02-08 18:14:02 +0000

    Now using the fish and plankton abundances from final time step which is much closer to the steady-state and does not have the ridiculous overabundance of plankton that is there in the initial configuration.

--- R/Experimental Code/BuildingEnergyConvolutionEvaluator.R
+++ R/Experimental Code/BuildingEnergyConvolutionEvaluator.R
@@ -14,12 +14,13 @@ library(reshape2)
 
 params <- set_community_model(z0 = 0.1, f0 = 0.7, alpha = 0.2, recruitment = 4e7,
                               sigma = 2, no_w=100)
-object <- params 
-sim <- project(params, t_max=1)
+sim <- project(params)
+object <- sim@params 
 
 # extract n_pp and n from sim object 
-n_pp <- sim@n_pp[1, ]
-n <- sim@n[1, , ]
+nt <- dim(sim@n_pp)[1]  # index of last time step
+n_pp <- sim@n_pp[nt, ]
+n <- sim@n[nt, , ]
 # sim@n[time,species, weight ]
 # we need to get species index back even though there is only one species
 dim(n) <- c(1, length(n))
@@ -28,25 +29,16 @@ mizerResult <- rowSums(sweep(object@pred_kernel,3,object@dw_full*object@w_full*n
 #presumably the above over estimates, because it is based on a left-Riemman sum. The following uses right-Riemman sum, provided 
 # that the last entry integrated via rowSums in `mizerResult` is negligable
 
-
 Presweep <- array(dim=c(1,dim(object@pred_kernel)[2],(dim(object@pred_kernel)[3]-1)))
 Presweep[1,,] <- object@pred_kernel[,,-1]
 mizerUnderEstimate2 <- rowSums(sweep(Presweep,3,(object@dw_full[-(length(object@dw_full))])*(object@w_full[-1])*(n_pp[-1]),"*", check.margin=FALSE),dims=2)
 
-
-
-
-
 ######################
 w0 <- object@w[1]
 
-length(B)
-
-dim(AA)
-
 Beta <- log(object@species_params$beta)
 sigma <- object@species_params$sigma
-wFull <- params@w_full
+wFull <- object@w_full
 xFull <- log(wFull)
 dx <- xFull[2]-xFull[1]
 s <- exp(-(xFull - xFull[1] - Beta)^2/(2*sigma^2))
