f87f1f2352fff3f67a0e0c3104240774037a59e1
unknown <richardsouthwell254@gmail.com>
2017-02-15 14:08:00 +0000

    Added the new code into MizerParams-classNewSlots.R which now seems to work ok, next I should refer to to the new smatM slot in project_methodsFFT2.R

--- R/MizerParams-classNewSlots.R
+++ R/MizerParams-classNewSlots.R
@@ -610,7 +610,48 @@ setMethod('MizerParams', signature(object='data.frame', interaction='matrix'),
             
             res@smat <- smat 
             
-            res@smatM <- smat
+            ##################
+            
+            noSpecies <- dim(res@interaction)[1]
+            no_Pvec <- rep(0, noSpecies)
+            Pvec <- rep(0, noSpecies)
+            for (j in 1:noSpecies){
+              Beta <- log(res@species_params$beta)[j]
+              sigma <- res@species_params$sigma[j]
+              Delta <- dx*round(min(2*sigma, Beta)/dx)
+              Beta <- dx*round(Beta/dx)
+              Delta <- Beta
+              min_cannibal <- 1+floor((Beta-Delta)/dx)
+              P <- x[length(x)] + 2*Delta
+              no_P <- 1+ceiling(P/dx)  # P/dx should already be integer 
+              x_P <- (1:no_P)*dx#+Beta-Delta-dx
+              no_Pvec[j] <- no_P
+              Pvec[j] <- P
+            }
+            no_P <- max(no_Pvec)
+            P <- max(Pvec)
+            # # # 
+            
+            phiMortality <- matrix(0,nrow = noSpecies, ncol = no_P)
+            for (j in 1:noSpecies){
+              Beta <- log(res@species_params$beta)[j]
+              sigma <- res@species_params$sigma[j]
+              Delta <- dx*round(min(2*sigma, Beta)/dx)
+              Beta <- dx*round(Beta/dx)
+              Delta <- Beta
+              min_cannibal <- 1+floor((Beta-Delta)/dx)
+              #  P <- x[length(x)] + 2*Delta
+              #  no_P <- 1+ceiling(P/dx)  # P/dx should already be integer 
+              x_P <- (1:no_P)*dx#+Beta-Delta-dx
+              phi <- rep(0, length(x_P))
+              phi[abs(x_P+Beta-P)<Delta] <- exp(-(x_P[abs(x_P+Beta-P)<Delta] + Beta - P)^2/(2*sigma^2)) 
+              phiMortality[j, 1:length(phi)] <- phi
+            }
+            #####################
+            
+            res@smatM <- phiMortality
+            
+            ###res@smatM <- smat
             
             
             # Could maybe improve this. Pretty ugly at the moment
