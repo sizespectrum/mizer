52fda7dfb235c743b0eae251f3b4ff95a5507eb7
gustavdelius <gustav.delius@gmail.com>
2017-12-05 13:37:01 +0100

    No need to clone matrix passed as argument before we can write to it in C++

--- src/inner_project_loop.cpp
+++ src/inner_project_loop.cpp
@@ -7,11 +7,10 @@ NumericMatrix inner_project_loop(int no_sp, int no_w,
                 NumericMatrix n, NumericMatrix A, NumericMatrix B,
                 NumericMatrix S, NumericVector w_min_idx) {
     
-    NumericMatrix nn = clone(n);
     for (int i = 0; i < no_sp; i++) {
         for (int j = w_min_idx[i]; j < no_w; j++) {
-            nn(i,j) = (S(i,j) - A(i,j)*nn(i,j-1)) / B(i,j);
+            n(i,j) = (S(i,j) - A(i,j)*n(i,j-1)) / B(i,j);
         }
     }
-    return nn;
+    return n;
 }
