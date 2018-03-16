c1de4ef857526f1148848203114baad7f195fe59
Richard <richardsouthwell254@gmail.com>
2018-03-16 09:52:49 +0000

    #19 Deleting extra files ready for pull request

--- inst/wrapper_test.R
+++
@@ -1,6 +0,0 @@
-params2 <- set_scaling_model()
-
-t_max <- 5
-sim <- project(params2, t_max=t_max, dt=0.01, t_save=t_max/100 ,effort = 0, 
-               initial_n = params2@initial_n, initial_n_pp = params2@initial_n_pp)
-plot(sim)
