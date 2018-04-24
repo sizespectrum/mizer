79829548502c54dc31a4ffa12c298b32cb7b58e4
Gustav Delius <gustav.delius@gmail.com>
2018-04-24 15:57:29 +0100

    The app now allows comparison also of spawning stock biomass. For this I made changes to plots.R: * added getSSBFrame, * added y_ticks argument

--- inst/shiny/compare_trait_models/app.R
+++ inst/shiny/compare_trait_models/app.R
@@ -5,44 +5,66 @@ library(devtools)
 library(progress)
 
 server <- function(input, output, session) {
+    
+    params_trait <- reactive({
+        set_trait_model(min_w_inf = 10, max_w_inf = 1e4)
+    })
 
-    sim_trait <- reactive({
+    sim_trait_initial <- reactive({
         # Create a Progress object
         progress <- shiny::Progress$new(session)
         # Close the progress when this reactive exits (even if there's an error)
         on.exit(progress$close())
 
-        params <- set_trait_model(min_w_inf = 10, max_w_inf = 1e5,
-                                  knife_edge_size = input$knife_edge_size)
-        sim_trait_initial <- project(params_trait, t_max = 50, effort = 0)
-        project(params_trait, 
-                initial_n = sim_trait_initial@n[dim(sim_trait_initial@n)[1],,],
-                initial_n_pp = sim_trait_initial@n_pp[dim(sim_trait_initial@n_pp)[1],], 
+        project(params_trait(), t_max = 100, effort = 0)
+    })
+    
+    sim_trait <- reactive({
+        # Create a Progress object
+        progress <- shiny::Progress$new(session)
+        on.exit(progress$close())
+        
+        sti <- sim_trait_initial()
+        params <- params_trait()
+        params@species_params$knife_edge_size = exp(input$log_knife_edge_size)
+        project(params, 
+                initial_n = sti@n[dim(sti@n)[1],,],
+                initial_n_pp = sti@n_pp[dim(sti@n_pp)[1],], 
                 t_max = 15, effort = input$effort, 
                 shiny_progress = progress)
-        })
+    })
     
     sim <- reactive({
         # Create a Progress object
         progress <- shiny::Progress$new(session)
-        # Close the progress when this reactive exits (even if there's an error)
         on.exit(progress$close())
         
         params <- set_scaling_model(
-            min_w_inf = 10, max_w_inf = 1e5,
-            knife_edge_size = input$knife_edge_size,
+            min_w_inf = 10, max_w_inf = 1e4,
+            knife_edge_size = exp(input$log_knife_edge_size),
             min_egg = 1e-4, min_w_mat = 10^(0.4), rfac = Inf,
-            kappa = 0.005)
+            kappa = 0.01)
+        
         project(params, t_max = 15, effort = input$effort, 
                 shiny_progress = progress)
     })
-
+    
+    output$plotSSB <- renderPlot({
+        b_trait <- getSSBFrame(sim_trait())
+        b <- getSSBFrame(sim())
+        display_frames(b_trait, b)})
+    
     output$plotBiomass <- renderPlot({
         b_trait <- getBiomassFrame(sim_trait())
         b <- getBiomassFrame(sim())
         display_frames(b_trait, b)})
-    output$plotYield <- renderPlot({plotYield(sim_trait(), sim())})
-    output$plotSpectra <- renderPlot({plotSpectra(sim())})
+    
+    output$plotYield <- renderPlot({
+        if (input$effort > 0) {
+            plotYield(sim_trait(), sim())
+        } else
+            ggplot()
+        })
 
 } #the server
 
@@ -55,15 +77,16 @@ ui <- fluidPage(
         
         sidebarPanel(
             sliderInput("effort", "Fishing effort",
-                        value=0.4, min=0, max=2, step=0.1),
-            sliderInput("knife_edge_size", "Minimum fishing size",
-                        value=100, min=10, max=1000, ticks=FALSE)
+                        value=0, min=0, max=2, step=0.1),
+            sliderInput("log_knife_edge_size", "Log10 of minimum fishing size",
+                        value=2, min=1, max=3, ticks=FALSE)
         ), #endsidebarpanel
         
         mainPanel(
             tabsetPanel(type = "tabs",
-                tabPanel("Biomass", plotOutput("plotBiomass")),
-                tabPanel("Yield", plotOutput("plotYield"))
+                tabPanel("SSB", plotOutput("plotSSB")),
+                tabPanel("Yield", plotOutput("plotYield")),
+                tabPanel("Total Biomass", plotOutput("plotBiomass"))
             )
         )#end mainpanel
     )# end sidebarlayout
