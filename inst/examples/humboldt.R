dc7e6cb04595b583497e62a37bbf628bec579460
unknown <richardsouthwell254@gmail.com>
2018-07-16 16:41:35 +0100

    .

--- inst/examples/humboldt.R
+++ inst/examples/humboldt.R
@@ -76,7 +76,7 @@ getAgesAtMaturity <- function(p){
 }
 
 
-age_at_maturity <- getAgesAtMaturity(p)
+#age_at_maturity <- getAgesAtMaturity(p)
 
 #((p@species_params$w_mat[i]/p@species_params$w_inf[i])^(1/p@species_params$b[i]))
 
@@ -155,43 +155,94 @@ age_at_maturity_mizer(p)
 target_ages <- getAgesAtMaturity(p)
 target_ages
 qq <- retune_h_for_growth(p,target_ages)
-qq <- steady(qq, effort = effort, 
-             t_max = 100, tol = 1e-2)
-age_at_maturity_mizer(qq)
-qqq <- retune_h_for_growth(p,2*target_ages)
-age_at_maturity_mizer(qqq)
+my_h <- qq@species_params$h
+################################################### rebuild params object with new h
+
+pq <- setBackground(
+  set_scaling_model(min_w_pp = 1e-12,
+                    no_sp = 10, no_w = 400, min_w_inf = 2, max_w_inf = 6e5,
+                    min_egg = 1e-4, min_w_mat = 2 / 10^0.6, 
+                    lambda = 2.12,
+                    knife_edge_size = Inf, h = my_h[1])
+)
+
+all_efforts <- c(0, 1.4, 1.1)
+names(all_efforts) <- c("knife_edge_gear", "sigmoid_gear", "sigmoid_gear_Anchovy")
+effort <- all_efforts[1:2]
+for (i in (1:no_sp)) {
+  if (params_data$species[i] == "Anchovy") {
+    effort <- c(effort, all_efforts[3])
+  }
+  a_m <- params_data$a2[i]
+  b_m <- params_data$b2[i]
+  L_inf_m <- params_data$Linf[i]
+  L_mat <- params_data$Lmat[i]
+  if (params_data$species[i] == "Anchovy") {
+    gear <- "sigmoid_gear_Anchovy"
+  } else {
+    gear <- "sigmoid_gear"
+  }
+  species_params <- data.frame(
+    species = as.character(params_data$species[i]),
+    w_min = params_data$Wegg[i],
+    w_inf = params_data$w_inf[i],
+    w_mat = params_data$w_mat[i],
+    beta = params_data$beta[i],
+    sigma = log(params_data$sigma[i]),
+    z0 = 0,
+    alpha = 0.6,
+    erepro = 0.1, # unknown, determined later
+    sel_func = "sigmoid_length",
+    gear = gear,
+    l25 = l25[i],
+    l50 = l50[i],
+    k = 0,
+    k_vb = params_data$k_vb[i],
+    a = a_m,
+    b = b_m,
+    h = my_h[i]
+  )
+  
+  pq <- addSpecies(pq, species_params, effort = effort, rfac=Inf)
+}
+
+# Run to steady state
+pq <- steady(pq, effort = effort, t_max = 100,  tol = 1e-3)
+
+
+################## end rebuild of pq
+
+
+age_at_maturity_mizer(p)
+target_ages <- getAgesAtMaturity(p)
+target_ages
+age_at_maturity_mizer(pq)
+
+## ! # why is age_at_maturity_mizer(pq)[last] == NA ??
+
+
+
+#qq <- steady(qq, effort = effort, 
+#             t_max = 100, tol = 1e-2)
+#age_at_maturity_mizer(qq)
+#qqq <- retune_h_for_growth(p,2*target_ages)
+#age_at_maturity_mizer(qqq)
 
 ############################## although I changed the value of h of qq, why does it still give 
 # the same growth rate. I guess the object needs to be reconstructed so that the change in h takes effect.
 
-qq <- retune_h_for_growth(p,age_at_maturity)
-plotGrowthCurves(qq, species="Sardine")
-qq <- steady(qq, effort = effort, 
-             t_max = 100, tol = 1e-2)
-plotGrowthCurves(qq, species="Sardine")
-
 XX <- get_mizer_growth_rates(p)
-plot(XX$times,XX$weights[3,])
-
-
-age_at_maturity[7:length(age_at_maturity)]
-age_at_maturity_mizer(p)
-age_at_maturity_mizer(qq)
 
 # the third entries of these agree, so we have set 
 # the age at maturity to the value age_at_maturity we want, 
 # but looking at the black curve below, it appears there 
 # might be an error with the way we extracted this from VB
 
-plotGrowthCurves(qq, species="Sardine")
-
-
-
 # Need to make these plots etc. have universal format
 # need to agree with the growth rates I observe
 # need to check 
 
-###############################################
+############################################### continue with rest of code
 
 humboldt_params <- p
 devtools::use_data(humboldt_params)
