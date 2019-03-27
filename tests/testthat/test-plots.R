fadb521b543db19372f610e57070d8e41b4e4fa2
Gustav Delius <gustav.delius@gmail.com>
2019-03-27 12:48:13 +0000

    Don't want changes in environment to trigger failing of tests that plots have not changed

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -8,26 +8,42 @@ sim <- project(params, effort = 1, t_max = 3, dt = 1, t_save = 1)
 species <- c("Cod", "Haddock")
 
 test_that("plots have not changed", {
-expect_known_value(plotBiomass(sim, species = species, total = TRUE,
-                               start_time = 0, end_time = 2.8,
-                               y_ticks = 4),
-                   "values/plotBiomass")
-expect_known_value(plotYield(sim, species = species, total = TRUE),
-                   "values/plotYield")
-expect_known_value(plotYieldGear(sim, species = species), "values/plotYieldGear")
-expect_known_value(plotSpectra(sim, species = species, total = TRUE,
-                               time_range = 1:3, power = 2,
-                               ylim = c(1e6, NA)),
-                   "values/plotSpectra")
-expect_known_value(plotFeedingLevel(sim, species = species, time_range = 1:3),
-                   "values/plotFeedingLevel")
-expect_known_value(plotM2(sim, species = species, time_range = 1:3),
-                   "values/plotM2")
-expect_known_value(plotFMort(sim, species = species, time_range = 1:3),
-                   "values/plotFMort")
-expect_known_value(plotGrowthCurves(sim, species = species, percentage = TRUE,
-                                    max_age = 50),
-                   "values/plotGrowthCurves")
+p <- plotBiomass(sim, species = species, total = TRUE,
+                 start_time = 0, end_time = 2.8,
+                 y_ticks = 4)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotBiomass")
+
+p <- plotYield(sim, species = species, total = TRUE)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotYield")
+
+p <- plotYieldGear(sim, species = species)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotYieldGear")
+
+p <- plotSpectra(sim, species = species, total = TRUE,
+                 time_range = 1:3, power = 2,
+                 ylim = c(1e6, NA))
+p$plot_env <- NULL
+expect_known_value(p, "values/plotSpectra")
+
+p <- plotFeedingLevel(sim, species = species, time_range = 1:3)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotFeedingLevel")
+
+p <- plotM2(sim, species = species, time_range = 1:3)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotM2")
+
+p <- plotFMort(sim, species = species, time_range = 1:3)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotFMort")
+
+p <- plotGrowthCurves(sim, species = species, percentage = TRUE,
+                      max_age = 50)
+p$plot_env <- NULL
+expect_known_value(p, "values/plotGrowthCurves")
 })
 
 test_that("plot function do not throw error", {
