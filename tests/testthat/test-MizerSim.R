306cfcf5043759cf103ae70074a381bdb661f09e
Gustav Delius <gustav.delius@gmail.com>
2019-03-28 09:19:14 +0000

    Adding a test

--- tests/testthat/test-MizerSim.R
+++ tests/testthat/test-MizerSim.R
@@ -1,10 +1,10 @@
 context("MizerSim constructor dimension checks")
+data(NS_species_params_gears)
+data(inter)
+params <- set_multispecies_model(NS_species_params_gears, inter)
 
 test_that("basic constructor sets dimensions properly",{
     # check dimension against t input arguments
-    data(NS_species_params_gears)
-    data(inter)
-    params <- set_multispecies_model(NS_species_params_gears, inter)
     # Make MizerSims with t_max and t_save
     t_max <- 5
     t_save <- 1
@@ -18,7 +18,7 @@ test_that("basic constructor sets dimensions properly",{
     t_max <- 4
     t_save <- 2
     sim <- MizerSim(params, t_max = t_max, t_save = t_save)
-    expect_that(dim(sim@effort)[1], equals(1+ t_max / t_save))
+    expect_that(dim(sim@effort)[1], equals(1 + t_max / t_save))
     expect_that(dimnames(sim@effort)[[1]], is_identical_to(as.character(seq(from = 0, to = t_max, by = t_save))))
     expect_that(all(dimnames(sim@effort)[[2]] %in% dimnames(params@selectivity)$gear), is_true())
     expect_that(dim(sim@n)[1], equals(1 + (t_max / t_save)))
@@ -36,9 +36,10 @@ test_that("basic constructor sets dimensions properly",{
     expect_that(dim(sim@n)[1], equals(length(t_dimnames)))
     expect_that(dimnames(sim@n)[[1]], is_identical_to(as.character(t_dimnames)))
 
-    # Check error if t_dimnames is not numeric
-    expect_that(MizerSim(params, t_dimnames = c("x","y","z")), throws_error())
-    expect_that(MizerSim(params, t_dimnames = as.character(1:3)), throws_error())
+    # Check error if t_dimnames is not numeric or not sorted
+    expect_error(MizerSim(params, t_dimnames = c("x","y","z")))
+    expect_error(MizerSim(params, t_dimnames = as.character(1:3)))
+    expect_error(MizerSim(params, t_dimnames = 3:1))
 })
 
 
