370f3b58ed7a514707b0592ad114154117fb859d
Gustav Delius <gustav.delius@gmail.com>
2019-04-02 12:25:33 +0100

    Starting separate testthat context for regression tests for the paramter setting functions

--- tests/testthat/test-MizerParams.R
+++ tests/testthat/test-MizerParams.R
@@ -98,17 +98,3 @@ test_that("w_min_idx is being set correctly", {
     expect_equal(params@w_min_idx[7], max(which(params@w <= 10)), 
                  check.names = FALSE)
 })
-
-
-## setResourceEncounter ----
-test_that("setResourceEncounter works", {
-    resource_dynamics <-
-        list("detritus" = function(params, n, n_pp, B, rates, dt, ...) B["detritus"],
-             "carrion" = function(params, n, n_pp, B, rates, dt, ...) B["carrion"])
-    species_params <- NS_species_params
-    species_params$rho_detritus <- 1:no_sp
-    species_params$rho_carrion <- no_sp:1
-    params <- MizerParams(species_params, resource_dynamics = resource_dynamics)
-    expect_equal(params@rho[2, 1, 1], 2 * params@w[1]^params@n)
-    expect_equal(params@rho[2, 2, 1], (no_sp - 1) * params@w[1]^params@n)
-})
