0a2fbff97d21a3a6651e6e4728af9893b07b1b75
Gustav Delius <gustav.delius@gmail.com>
2019-04-06 19:08:33 +0100

    Added test for displayFrame() and getBiomassFrame()

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -5,8 +5,10 @@ data(NS_species_params_gears)
 data(inter)
 params <- set_multispecies_model(NS_species_params_gears, inter, no_w = 30)
 sim <- project(params, effort = 1, t_max = 3, dt = 1, t_save = 1)
+sim0 <- project(params, effort = 0, t_max = 3, dt = 1, t_save = 1)
 species <- c("Cod", "Haddock")
 
+# plots have not changed ----
 test_that("plots have not changed", {
 p <- plotBiomass(sim, species = species, total = TRUE,
                  start_time = 0, end_time = 2.8,
@@ -44,13 +46,25 @@ p <- plotGrowthCurves(sim, species = species, percentage = TRUE,
                       max_age = 50)
 p$plot_env <- NULL
 expect_known_value(p, "values/plotGrowthCurves")
+
+p <- displayFrames(getBiomassFrame(sim0, 
+                                   species = species,
+                                   start_time = 1,
+                                   total = TRUE), 
+                   getBiomassFrame(sim,
+                                   end_time = 3,
+                                   ylim = c(1e12)),
+                   params)
+p$plot_env <- NULL
+expect_known_value(p, "values/displayFrames")
 })
 
 test_that("plot function do not throw error", {
     expect_error(plot(sim, species = species, wlim = c(10, 100), w_min = 10), NA)
 })
 
-test_that("plotly function do not throw error", {
+# plotly functions do not throw error
+test_that("plotly functions do not throw error", {
     expect_error(plotlyBiomass(sim, species = species), NA)
     expect_error(plotlyFeedingLevel(sim, species = species), NA)
     expect_error(plotlyYield(sim, species = species), NA)
