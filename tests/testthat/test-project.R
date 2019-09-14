c8b3be90ca682760cde630dcde5f034a4b2fcf11
Gustav Delius <gustav.delius@gmail.com>
2019-09-14 13:46:02 +0100

    Test that example params objects are at steady state, instead of testing against previous numerical values.

--- tests/testthat/test-project.R
+++ tests/testthat/test-project.R
@@ -365,22 +365,22 @@ test_that("Final result the same when called with sim or params", {
 test_that("Example params objects are projected correctly", {
   sim <- project(Baltic_params, t_max = 0.3, t_save = 0.3,
                  effort = c(small = 0.3, medium = 0.3, large = 0.7))
-  expect_known_value(sim@n[2, 3, ], "values/projectBaltic")
+  expect_equal(sim@n[2, , ], Baltic_params@initial_n[, ])
   
   sim <- project(Barents_params, t_max = 0.3, t_save = 0.3,
                  effort = c(small = 1.1, medium = 0.5, large = 0.75))
-  expect_known_value(sim@n[2, 3, ], "values/projectBarents")
+  expect_equal(sim@n[2, , ], Barents_params@initial_n[, ])
   
   sim <- project(Benguela_params, t_max = 0.3, t_save = 0.3,
                  effort = c(small = 0.13, medium = 0.05, large = 0.45))
-  expect_known_value(sim@n[2, 3, ], "values/projectBenguela")
+  expect_equal(sim@n[2, , ], Benguela_params@initial_n[, ])
   
   sim <- project(NEUSCS_params, t_max = 0.3, t_save = 0.3,
                  effort = c(small = 0.4, medium = 0.3, large = 0.25))
-  expect_known_value(sim@n[2, 3, ], "values/projectNEUSCS")
+  expect_equal(sim@n[2, , ], NEUSCS_params@initial_n[, ])
   
   sim <- project(NorthSea_params, t_max = 0.3, t_save = 0.3,
                  effort = c(small = 0.6, medium = 0.6, large = 1.25))
-  expect_known_value(sim@n[2, 3, ], "values/projectNorthSea")
+  expect_equal(sim@n[2, , ], NorthSea_params@initial_n[, ])
   
 })
