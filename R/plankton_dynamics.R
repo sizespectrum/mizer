5ee2ac58cbd40d69f868cf76024035933955989d
Gustav W Delius <gustav.delius@gmail.com>
2019-12-13 16:51:33 +0000

    Pass time (#121)
    Pass current time to resource dynamics functions. This enables users to implement time-dependent resource dynamics.

--- R/plankton_dynamics.R
+++ R/plankton_dynamics.R
@@ -5,6 +5,7 @@
 #' @param n_pp A vector of the plankton abundance by size
 #' @param B The biomass of detritus
 #' @param rates A list of rates as returned by [getRates()]
+#' @param t The current time
 #' @param dt Time step
 #' @param ... Unused
 #'   
@@ -18,7 +19,7 @@
 #' params <- set_multispecies_model(NS_species_params_gears, inter,
 #'                              plankton_dyn = plankton_semichemostat)
 #' }
-plankton_semichemostat <- function(params, n, n_pp, B, rates, dt, ...) {
+plankton_semichemostat <- function(params, n, n_pp, B, rates, t, dt, ...) {
     # Dynamics of plankton spectrum uses a semi-chemostat model (de Roos - ask Ken)
     # We use the exact solution under the assumption of constant mortality during timestep
     tmp <- params@rr_pp * params@cc_pp / (params@rr_pp + rates$plankton_mort)
