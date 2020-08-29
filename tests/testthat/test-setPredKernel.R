0092a1a73091d6cf40f201008141a0eb54532239
Gustav Delius <gustav.delius@gmail.com>
2020-08-29 18:27:14 +0200

    Check for equal rather than identical, otherwise the test may fail on machines other than the one on which NS_params was created.

--- tests/testthat/test-setPredKernel.R
+++ tests/testthat/test-setPredKernel.R
@@ -3,8 +3,8 @@ no_sp <- nrow(params@species_params)
 
 ## setPredKernel ----
 test_that("setPredKernel works", {
-    expect_identical(setPredKernel(params), params)
-    expect_identical(setPredKernel(params, pred_kernel = NULL), 
+    expect_equal(setPredKernel(params), params)
+    expect_equal(setPredKernel(params, pred_kernel = NULL), 
                      params)
     params@species_params$pred_kernel_type <- "box"
     params@species_params$ppmr_min <- 2
--- tests/testthat/test-setSearchVolume.R
+++ tests/testthat/test-setSearchVolume.R
@@ -3,10 +3,10 @@ no_sp <- nrow(params@species_params)
 
 ## setSearchVolume ----
 test_that("setSearchVolume works", {
-    expect_identical(setSearchVolume(params, params@search_vol), params)
+    expect_equal(setSearchVolume(params, params@search_vol), params)
     params@species_params$gamma <- 2 * params@species_params$gamma
     p2 <- setSearchVolume(params)
-    expect_identical(2 * params@search_vol, p2@search_vol)
+    expect_equal(2 * params@search_vol, p2@search_vol)
 })
 test_that("Comment works on search volume", {
     comment(params@search_vol) <- "test"
