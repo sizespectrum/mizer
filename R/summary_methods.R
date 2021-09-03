sizespectrum/mizer

commit 0811708dd6e4e90e4e026fcee075d3bcc66162a6

When copying information from last time step don't just copy `n` and `n_pp` but use `setInitialValues()`. Otherwise other components are not copied.

R/summary_methods.R
@@ -329,13 +329,9 @@ getGrowthCurves <- function(object,
                             percentage = FALSE) {
     if (is(object, "MizerSim")) {
         params <- object@params
-        t <- dim(object@n)[1]
-        n <- object@n[t, , ]
-        n_pp <- object@n_pp[t, ]
+        params <- setInitialValues(params, object)
     } else if (is(object, "MizerParams")) {
         params <- validParams(object)
-        n <- object@initial_n
-        n_pp <- object@initial_n_pp
     } else {
         stop("The first argument to `getGrowthCurves()` must be a ",
              "MizerParams or a MizerSim object.")
@@ -347,7 +343,7 @@ getGrowthCurves <- function(object,
     age <- seq(0, max_age, length.out = 50)
     ws <- array(dim = c(length(species), length(age)),
                 dimnames = list(Species = species, Age = age))
-    g <- getEGrowth(params, n, n_pp)
+    g <- getEGrowth(params)
     for (j in seq_along(species)) {
         i <- idx[j]
         g_fn <- stats::approxfun(c(params@w, params@species_params$w_inf[[i]]),

R/plots.R
@@ -1073,10 +1073,7 @@ plotGrowthCurves <- function(object, species = NULL,
                 is.number(max_age))
     if (is(object, "MizerSim")) {
         params <- object@params
-        t <- dim(object@n)[1]
-        params@initial_n[] <- object@n[t, , ] # Designed to work also
-                                              # with single species
-        params@initial_n_pp <- object@n_pp[t, ]
+        params <- setInitialValues(params, object)
     } else if (is(object, "MizerParams")) {
         params <- validParams(object)
     }

commit 22c301062882e706fd6e95f34e87ff3bb791cbab

Fixing bug introduced in commit 0811708d

R/summary_methods.R
@@ -338,8 +338,8 @@ getGrowthCurves <- function(object,
     }
     species <- valid_species_arg(params, species)
     # reorder list of species to coincide with order in params
-    idx <- which(dimnames(n)$sp %in% species)
-    species <- dimnames(n)$sp[idx]
+    idx <- which(params@species_params$species %in% species)
+    species <- params@species_params$species[idx]
     age <- seq(0, max_age, length.out = 50)
     ws <- array(dim = c(length(species), length(age)),
                 dimnames = list(Species = species, Age = age))