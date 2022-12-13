2d148d669b6f5c3df1532bff75502de2ceff2092
Gustav Delius <gustav.delius@gmail.com>
2022-12-13 16:04:57 +0100

    Extend some tests.

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -7,6 +7,11 @@ test_that("l2w works", {
     # call with wrong 2nd argument
     expect_error(l2w(2, 4),
                  "The second argument must be either ")
+    # call with wrong 1st argument
+    expect_error(l2w("a", NS_species_params),
+                 "l is not a numeric or integer vector")
+    expect_error(l2w(1:2, NS_species_params),
+                 "The length of 'l'")
 })
 
 test_that("w2l works", {
@@ -15,4 +20,12 @@ test_that("w2l works", {
     expect_identical(w2l(0.08, NS_species_params), rep(2, no_sp))
     # call with params
     expect_identical(w2l(0.08, NS_params), rep(2, no_sp))
+    # call with wrong 1st argument
+    expect_error(w2l("a", NS_species_params),
+                 "w is not a numeric or integer vector")
+    expect_error(w2l(1:2, NS_species_params),
+                 "The length of 'w'")
+    # w2l should do the inverse of l2w
+    expect_equal(w2l(l2w(2, NS_species_params), NS_species_params),
+                 rep(2, no_sp))
 })
