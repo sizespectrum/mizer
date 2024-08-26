d34f555fb1653adddfb89d019dcf3edcb3b4b929
Gustav Delius <gustav.delius@gmail.com>
2024-08-26 22:18:52 +0100

    Small documentation tweak

--- R/helpers.R
+++ R/helpers.R
@@ -18,9 +18,11 @@ different <- function(a, b) {
 
 #' Length-weight conversion
 #' 
-#' Convert between length and weight using the relationship
-#' \deqn{w = a l^b}
-#' where `a` and `b` are taken from the species parameter data frame.
+#' For each species, convert between length and weight using the relationship
+#' \deqn{w_i = a_i l_i^{b_i}}{w_i = a_i l_i^b_i} or 
+#' \deqn{l_i = (w_i / a_i)^{1/b_i}}{l_i = (w_i / a_i)^{1/b_i}
+#' where `a` and `b` are taken from the species parameter data frame and 
+#' \eqn{i}{i} is the species index.
 #' 
 #' This is useful for converting a length-based species parameter to a 
 #' weight-based species parameter.
@@ -28,10 +30,10 @@ different <- function(a, b) {
 #' If any `a` or `b` parameters are missing the default values `a = 0.01` and
 #' `b = 3` are used for the missing values.
 #' 
-#' @param l Lengths in cm. Either a single number or a vector with one number
-#'   for each species.
-#' @param w Weights in grams. Either a single number or a vector with one number
-#'   for each species.
+#' @param l Lengths in cm. Either a single number used for all species or a
+#'   vector with one number for each species.
+#' @param w Weights in grams. Either a single number used for all species or a
+#'   vector with one number for each species.
 #' @param params A species parameter data frame or a MizerParams object.
 #' @return A vector with one entry for each species. `l2w()` returns a vector
 #' of weights in grams and `w2l()` returns a vector of lengths in cm.
