a420dc90da404a6154d98539e33a07171639a939
Gustav Delius <gustav.delius@gmail.com>
2024-10-16 16:12:09 +0100

    Incorporating `validSim()` from mizerExperimental.

--- R/MizerSim-class.R
+++ R/MizerSim-class.R
@@ -243,8 +243,10 @@ MizerSim <- function(params, t_dimnames = NA, t_max = 100, t_save = 1) {
 
 #' Validate MizerSim object and upgrade if necessary
 #' 
-#' Checks that the given MizerSim object is valid and upgrades it if
-#' necessary.
+#' Checks that the given MizerSim object is valid and upgrades it if necessary.
+#' Checks whether any abundances are non-finite and if any are found, a warning
+#' is issued and the simulation is truncated at the last time step where all
+#' results are finite.
 #' 
 #' Occasionally, during the development of new features for mizer, the
 #' \linkS4class{MizerSim} class or the \linkS4class{MizerParams} class gains
@@ -274,7 +276,43 @@ validSim <- function(sim) {
         # Always validate the params object, even if no upgrading was required.
         sim@params <- validParams(sim@params)
     }
+    
+    if (!all(is.finite(sim@n))) {
+        inf_idx <- which(!is.finite(sim@n), arr.ind = TRUE)
+        max_t_idx <- min(inf_idx[, 1]) - 1
+        max_t <- as.numeric(dimnames(sim@n)$time[max_t_idx])
+        warning("The simulation failed to work beyond time = ", max_t)
+        # we can't use drop = FALSE because we do want to drop time dimension.
+        n <- sim@n[max_t_idx, , ]
+        dim(n) <- dim(sim@n)[2:3]
+        rates <- getRates(sim@params,
+                          n = n,
+                          n_pp = sim@n_pp[max_t_idx, ],
+                          n_other = sim@n_pp[max_t_idx, ],
+                          effort = sim@effort[max_t_idx, ],
+                          t = max_t)
+        inf_rates <- sapply(rates, function(x) any(!is.finite(x)))
+        if (any(inf_rates)) {
+            warning("The following rates failed to be finite: ",
+                    paste(names(rates)[inf_rates], collapse = ", "), ".")
+        }
+        sim <- truncateSim(sim, end_time = max_t)
+    }
     validObject(sim)
+    sim
+}
+
+truncateSim <- function(sim, end_time) {
+    assert_that(is(sim, "MizerSim"))
+    times <- dimnames(sim@n)$time
+    if (!(end_time %in% times)) {
+        stop("end_time = ", end_time, " is not a valid time contained in the simulation.")
+    }
+    t_idx <- match(end_time, times)
+    sim@n <- sim@n[1:t_idx, , , drop = FALSE]
+    sim@n_pp <- sim@n_pp[1:t_idx, , drop = FALSE]
+    sim@n_other <- sim@n_other[1:t_idx, , drop = FALSE]
+    sim@effort <- sim@effort[1:t_idx, , drop = FALSE]
     sim
 }
 
--- tests/testthat/test-MizerSim-class.R
+++ tests/testthat/test-MizerSim-class.R
@@ -47,3 +47,16 @@ test_that("basic constructor sets dimensions properly", {
                  "The t_dimnames argument should be increasing")
 })
 
+test_that("validSim works", {
+    sim <- project(NS_params, t_max = 0.2, t_save = 0.1)
+    sim@n[3, 1, 1] <- Inf
+    expect_warning(simt <- validSim(sim),
+                   "The simulation failed to work beyond time = 0.1")
+    expect_equal(dim(simt@n), c(2, 12, 100))
+    expect_equal(dim(simt@n_pp), c(2, 226))
+    expect_equal(dim(simt@effort), c(2, 4))
+    sim@n[2, 2, 2] <- NaN
+    expect_warning(simt <- validSim(sim),
+                   "The simulation failed to work beyond time = 0")
+    expect_equal(dim(simt@n), c(1, 12, 100))
+})
