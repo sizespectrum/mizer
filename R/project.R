2c4459589147d6d852a0fab7da31f314245b45e0
Gustav Delius <gustav.delius@gmail.com>
2026-01-30 19:26:25 +0000

    Refactor abundance projection into separate function.

--- R/project.R
+++ R/project.R
@@ -147,10 +147,9 @@ project.MizerParams <- function(object, effort,
                                 initial_n, initial_n_pp,
                                 append = TRUE,
                                 progress_bar = TRUE, ...) {
-    params <- object
+    params <- validParams(object)
     # Set and check initial values ----
     assert_that(t_max > 0)
-    params <- validParams(params)
     if (!missing(initial_n)) params@initial_n[] <- initial_n
     if (!missing(initial_n_pp)) params@initial_n_pp[] <- initial_n_pp
 
@@ -502,31 +501,8 @@ project_simple.MizerParams <-
             )
 
             # * Update species ----
-            # a_{ij} = - g_i(w_{j-1}) / dw_j dt
-            a[, idx] <- sweep(
-                -r$e_growth[, idx - 1, drop = FALSE] * dt, 2,
-                params@dw[idx], "/"
-            )
-            # b_{ij} = 1 + g_i(w_j) / dw_j dt + \mu_i(w_j) dt
-            b[] <- 1 + sweep(r$e_growth * dt, 2, params@dw, "/") + r$mort * dt
-            # S_{ij} <- N_i(w_j)
-            S[, idx] <- n[, idx, drop = FALSE]
-            # Update first size group of n
-            n[w_min_idx_array_ref] <-
-                (n[w_min_idx_array_ref] + r$rdd * dt /
-                    params@dw[params@w_min_idx]) /
-                    b[w_min_idx_array_ref]
-            # Update n
-            # for (i in 1:no_sp) # number of species assumed small, so no need to
-            #                      vectorize this loop over species
-            #     for (j in (params@w_min_idx[i]+1):no_w)
-            #         n[i,j] <- (S[i,j] - A[i,j]*n[i,j-1]) / B[i,j]
-            # This is implemented via Rcpp
-            n <- inner_project_loop(
-                no_sp = no_sp, no_w = no_w, n = n,
-                A = a, B = b, S = S,
-                w_min_idx = params@w_min_idx
-            )
+            n <- project_n(params, r, n, dt, a, b, S, idx, w_min_idx_array_ref,
+                           no_sp, no_w)
 
             # * Update time ----
             t <- t + dt
