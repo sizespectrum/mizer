40392c28a55ecd34fd9da9d8138bb9a183d96166
Gustav Delius <gustav.delius@gmail.com>
2026-02-16 05:07:26 +0000

    Add vignette to document numerical scheme

--- R/project_n.R
+++ R/project_n.R
@@ -1,4 +1,29 @@
-#' Advance abundance density by one time step
+#' Project values for first time step of Euler method
+#' 
+#' This is an internal function used by the user-facing `project()` function.
+#' It is of potential interest only to mizer extension authors.
+#' 
+#' @details
+#' The function calculates the abundance at the next time step using the
+#' McKendrick-von Foerster equation:
+#' \deqn{\frac{\partial N}{\partial t} + \frac{\partial g N}{\partial w} = -\mu N}
+#' which is solved using a semi-implicit upwind finite difference scheme.
+#' 
+#' @param params A \linkS4class{MizerParams} object.
+#' @param r A list of rates as returned by `mizerRates()`.
+#' @param n An array (species x size) with the number density at the current time step.
+#' @param dt Time step.
+#' @param a A matrix (species x size) used in the solver (transport term).
+#' @param b A matrix (species x size) used in the solver (diagonal term).
+#' @param S A matrix (species x size) used in the solver (source term).
+#' @param idx Index vector for size bins (excluding the first one).
+#' @param w_min_idx_array_ref Index vector for the start of the size spectrum for each species.
+#' @param no_sp Number of species.
+#' @param no_w Number of size bins.
+#' 
+#' @return The updated abundance density matrix `n`.
+#' @seealso \code{\link{project}}, \code{\link{mizerRates}}
+#' @concept helper
 #' @export
 project_n <- function(params, r, n, dt, a, b, S, idx, w_min_idx_array_ref,
                       no_sp, no_w) {
